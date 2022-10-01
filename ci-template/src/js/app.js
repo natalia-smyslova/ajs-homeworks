9e786e7be13cac43b6e36d68e23b425d0e9c4ca8
Smyslova Natalia <madlynx@lenta.ru>
2022-10-01 23:32:35 +0300

    first commit

--- ci-template/src/js/app.js
+++ ci-template/src/js/app.js
@@ -4,3 +4,31 @@ import sum from './basic';
 console.log('worked');
 
 console.log(sum([1, 2]));
+
+export function heroes({ name: name, health: health }) {
+    //условная конструкция
+    if (health === 100) {
+        return "healthy"
+    }
+    if (health < 100 && health > 15) {
+        return "wounded"
+    }
+    else return "critical"
+
+};
+
+
+
+export function orderOfHeroes(heroesList) {
+    let result = heroesList.sort(function (a, b) {
+        if (a.health > b.health) {
+            return -1;
+        }
+        if (a.health < b.health) {
+            return 1;
+        }
+        // a должно быть равным b
+        return 0 ;
+    })
+    return result
+};
